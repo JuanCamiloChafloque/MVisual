e18b2bcbe3a322c13a0ba99480fd35b0e513965e
Julio Mejía <julio.mejia@javeriana.edu.co>
2021-02-25 10:05:27 -0500

    Analyse Pixels apparently is working, the problem is the path that image picker generates

--- MVisual/src/screens/UploadImage/UploadImage.js
+++ MVisual/src/screens/UploadImage/UploadImage.js
@@ -1,10 +1,11 @@
 import React, {useState} from 'react';
-import {View, Text, Image, Button} from 'react-native';
+import {View, Image, Button, NativeModules} from 'react-native';
 import * as ImagePicker from 'react-native-image-picker';
 
+const imagenPrueba = '../../../assets/images/patient.png';
 const UploadImage = () => {
   const [photo, setPhoto] = useState(null);
-  handleChoosePhoto = () => {
+  const handleChoosePhoto = () => {
     const options = {
       noData: true,
     };
@@ -15,12 +16,37 @@ const UploadImage = () => {
       }
     });
   };
+  const analysePixels = () => {
+    if (photo != null) {
+      console.log('Uri: ', photo.uri);
+      NativeModules.Bitmap.getPixels(photo.uri)
+        .then((image) => {
+          console.log(image.width);
+          console.log(image.height);
+          console.log(image.hasAlpha);
+          for (let x = 0; x < image.width; x++) {
+            for (let y = 0; y < image.height; y++) {
+              const offset = image.width * y + x;
+              const pixel = image.pixels[offset];
+              console.log('Offset', offset);
+              console.log('Pixel', pixel);
+            }
+          }
+        })
+        .catch((err) => {
+          console.error(err);
+        });
+    } else {
+      console.warn('Upload an Image.....first');
+    }
+  };
   return (
     <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
       {photo && (
         <Image source={{uri: photo.uri}} style={{width: 300, height: 300}} />
       )}
       <Button title="Choose Photo" onPress={handleChoosePhoto} />
+      <Button title="Analyse Pixels" onPress={analysePixels} />
     </View>
   );
 };
